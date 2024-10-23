145673e89d0b653d27b5c9af9704f851943dc2ca
stephan <sreindl@innitiative.at>
2024-10-23 17:36:24 +0200

    Warnungen beseitigt

--- backtrader/cerebro.py
+++ backtrader/cerebro.py
@@ -641,21 +641,21 @@ class Cerebro(with_metaclass(MetaParams, object)):
         self.indicators.append((indcls, args, kwargs))
 
     def addanalyzer(self, ancls, *args, **kwargs):
-        '''
+        """
         Adds an ``Analyzer`` class to the mix. Instantiation will be done at
         ``run`` time
-        '''
+        """
         self.analyzers.append((ancls, args, kwargs))
 
     def addobserver(self, obscls, *args, **kwargs):
-        '''
+        """
         Adds an ``Observer`` class to the mix. Instantiation will be done at
         ``run`` time
-        '''
+        """
         self.observers.append((False, obscls, args, kwargs))
 
     def addobservermulti(self, obscls, *args, **kwargs):
-        '''
+        """
         Adds an ``Observer`` class to the mix. Instantiation will be done at
         ``run`` time
 
@@ -663,22 +663,23 @@ class Cerebro(with_metaclass(MetaParams, object)):
         buy/sell observer which observes individual datas.
 
         A counter-example is the CashValue, which observes system-wide values
-        '''
+        """
         self.observers.append((True, obscls, args, kwargs))
 
     def addstorecb(self, callback):
-        '''Adds a callback to get messages which would be handled by the
+        """
+        Adds a callback to get messages which would be handled by the
         notify_store method
 
         The signature of the callback must support the following:
 
-          - callback(msg, \*args, \*\*kwargs)
+          - callback(msg, *args, **kwargs)
 
         The actual ``msg``, ``*args`` and ``**kwargs`` received are
         implementation defined (depend entirely on the *data/broker/store*) but
         in general one should expect them to be *printable* to allow for
         reception and experimentation.
-        '''
+        """
         self.storecbs.append(callback)
 
     def _notify_store(self, msg, *args, **kwargs):
@@ -688,7 +689,8 @@ class Cerebro(with_metaclass(MetaParams, object)):
         self.notify_store(msg, *args, **kwargs)
 
     def notify_store(self, msg, *args, **kwargs):
-        '''Receive store notifications in cerebro
+        """
+        Receive store notifications in cerebro
 
         This method can be overridden in ``Cerebro`` subclasses
 
@@ -696,7 +698,7 @@ class Cerebro(with_metaclass(MetaParams, object)):
         implementation defined (depend entirely on the *data/broker/store*) but
         in general one should expect them to be *printable* to allow for
         reception and experimentation.
-        '''
+        """
         pass
 
     def _storenotify(self):
@@ -709,18 +711,19 @@ class Cerebro(with_metaclass(MetaParams, object)):
                     strat.notify_store(msg, *args, **kwargs)
 
     def adddatacb(self, callback):
-        '''Adds a callback to get messages which would be handled by the
+        """
+        Adds a callback to get messages which would be handled by the
         notify_data method
 
         The signature of the callback must support the following:
 
-          - callback(data, status, \*args, \*\*kwargs)
+          - callback(data, status, *args, **kwargs)
 
         The actual ``*args`` and ``**kwargs`` received are implementation
         defined (depend entirely on the *data/broker/store*) but in general one
         should expect them to be *printable* to allow for reception and
         experimentation.
-        '''
+        """
         self.datacbs.append(callback)
 
     def _datanotify(self):
